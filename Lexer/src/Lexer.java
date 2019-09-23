85a0915c1f1750b17fea926eb6220407770f80bf
Daniel Flores <Daniel.Flores03@student.csulb.edu>
2019-09-23 03:44:21 -0700

    lexer skeleton

---
+++ Lexer/src/Lexer.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Lexer {
+	ArrayList<Character> array;
+	int charCount; // keep track of current character from input stream
+	int peekCount; // keep track of character we are looking forward to
+	
+	// TODO map tokens to id numbers?
+	
+	// TODO deterministic finite automata using switch statements
+	
+	public Lexer(ArrayList<Character> array) {
+		this.array = array;
+	}
+	
+	public char peek() {
+		return array.get(charCount + 1);
+	}
+	
+	public void advance() {
+		peekCount ++;
+	}
+}
--- Lexer/src/Main.java
+++ Lexer/src/Main.java
@@ -1,8 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO open file stdin
+		// The name of the file to open.
+        String fileName = args[1];
+
+        try {
+        	File code = new File(fileName);
+        	Scanner sc  = new Scanner(code);
+        	// TODO store each char in ArrayList for future use?
+                    
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Unable to open file '" + fileName + "'");                
+        }
+        catch(IOException e) {
+            System.out.println("Error reading file '" + fileName + "'");                   
+            // e.printStackTrace();
+        }
 
 	}
 
